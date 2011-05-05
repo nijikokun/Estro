8351e60c98e7107ea62d534b443badaf13a8eada
Nijiko Yonskai <me@nijikokun.com>
2011-05-05 12:15:58 -0400

    Added Repeat, Reverse, Bind, Extract, Capitalize, Camelize; Rewrote the way Estro adds the methods as well.

--- estro.js
+++ estro.js
@@ -8,44 +8,28 @@
  * @license: GNUv3 Affero License <http://www.gnu.org/licenses/agpl-3.0.html>
  */
 
-if(!String.prototype.isEmpty){
-    String.prototype.isEmpty = function(){
-        return (!this ||this == '' || this == undefined || this == NaN || this == null || this.length < 1 || this == 'undefined' || (typeof this) == undefined || (typeof this) == NaN);
+(function(){
+    var methods = {
+        isEmpty: function(){
+            return (!this ||this === '' || this === undefined || this === NaN || this === null || this.length < 1 || this === 'undefined' || (typeof this) === undefined || (typeof this) === NaN);
+        },
+        startsWith: function (str) { return !this.indexOf(str); },
+        endsWith: function(str) { return this.indexOf(str, this.length - str.length) !== -1; },
+        contains: function(n) {  return this.indexOf(n) != -1; },
+        equals: function(str) { return (this == str); },
+        equalsIgnoreCase: function(str) { var o=this; return (o == str) ? true : ((!str.isEmpty()) && (str.length == o.length) && o.toLowerCase() == str.toLowerCase()); },
+        trim: function() { return this.replace(/^\s\s*/, '').replace(/\s\s*$/, ''); },
+        repeat: function(n){ return new Array(n?n+1:2).join(this); },
+        reverse: function () { return this.split('').reverse().join(''); },
+        capitalize: function() { var w=this.split(' '); for(var i=0;i<w.length;i++) { w[i] = w[i].charAt(0).toUpperCase()+w[i].substring(1).toLowerCase(); } return w.join(" "); },
+        camelize: function() { this.replace(/\-(\w)/g, function( $0, $1 ) { return $1.toUpperCase(); }); },
+        bind: function (d) { var m,o=this; while (m = /%\{\s*([^\}\s]+)\s*\}/.exec(o) ) { o = o.replace( m[0], d[m[1]] || '??' ); } return o; },
+        extract: function(rgx, n) {
+            n=(n === undefined)?0:n; if (!rgx.global) { return this.match(rgx)[n] || ''; } var m,e=[]; while((m=rgx.exec(this))) { e[e.length] = m[n] || ''; } return e;
+        }
     };
-}
-
-if(!String.prototype.startsWith){
-    String.prototype.startsWith = function (prefix) {
-        return !this.indexOf(prefix);
-    };
-}
-
-if(!String.prototype.endsWith) {
-    String.prototype.endsWith = function(suffix) {
-        return this.indexOf(suffix, this.length - suffix.length) !== -1;
-    };
-}
-
-if(!String.prototype.contains) {
-    String.prototype.contains = function(what) { 
-        return this.indexOf(what) != -1; 
-    };
-}
-
-if(!String.prototype.equals) {
-    String.prototype.equals = function(string) { 
-        return (this == string); 
-    };
-}
-
-if(!String.prototype.equalsIgnoreCase) {
-    String.prototype.equalsIgnoreCase = function(string) { 
-        return (this == string) ? true : ((string != null) && (string.length == this.length) && this.toLowerCase() == string.toLowerCase()); 
-    };
-}
-
-if(!String.prototype.trim) {
-    String.prototype.trim = function() {
-        return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-    };
-}
+    
+    for (var method in methods) {
+        if(!String.prototype[method])  String.prototype[method] = methods[method];
+    }
+})();
