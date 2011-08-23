dd104deeff5ab1d5a8b3141b5e9e5c75991bf505
Nijiko Yonskai <me@nijikokun.com>
2011-08-23 19:43:47 -0400

    Update to version 2.1

--- README.md
+++ README.md
@@ -31,13 +31,31 @@ Estro contains the following string extensions (Unless they already exist; Estro
   * string.endsWith(suffix) *Check to see if a string ends with the given string. Returns: Boolean*
   * string.contains(needle) *Check to see if a string contains a given string. Returns Boolean*
   * string.trim() *Trim the string of whitespace. Returns: String*
+  * string.trimLeft() *Trim the left of the string of whitespace. Returns: String*
+  * string.trimRight() *Trim the right of the string of whitespace. Returns: String*
+  * string.trimNumeric() *Returns: String*
+  * string.trimNonAlpha() *Returns: String*
+  * string.trimNonNumeric() *Returns: String*
+  * string.trimNonAlphaNumeric() *Returns: String*
   * string.isEmpty() *Check to see if the string is empty, and really empty. Returns Boolean*
+  * string.isBlank() *Check to see if the string is undefined, or null. Returns Boolean*
   * string.reverse() *Reverses string. IE: Hello -> olleH*
+  * string.remove(start, end) *Remove characters between the `start` and `end`. Returns String*
+  * string.insert(string, indice) *Insert string starting at Indice. Returns String*
+  * string.chop(amount) *Pop characters from end of the string. Amount defaults to 1. Returns String*
   * string.repeat(n) *Repeat string n times*
-  * string.capitalize() *Capitalize each word in the string and lower the rest. Returns String*
-  * string.camelize() *CamelCase string, hello-there -> helloThere. Returns String*
+  * string.capitalize() *Capitalize the first word. Returns String*
+  * string.uncapitalize() *Uncapitalize the first word. Returns String*
+  * string.capitalizeWords() *Capitalize each word in the string. Returns String*
+  * string.uncapitalize() *Uncapitalize each word in the string. Returns String*
+  * string.isUppercaseAt(indice) *Check to see if the string is uppercase at a certain character. Returns Boolean*
+  * string.isLowercaseAt(indice) *Check to see if the string is lowercase at a certain character. Returns Boolean*
+  * string.camelize() *CamelCase string, hello there -> helloThere. Returns String*
   * string.bind(data) *Small template layer. Ex: "Hi %{name}".bind({'name': 'Estro'});" Outputs: Hi Estro*
   * string.extract(regex, n) *Extract data into an array based on regex and group(n). Returns Array*
+  * string.toInt() *Converts the string into its numeric value. Returns Integer*
+  * string.toHash() *Converts the string it a hashcode, based on Java. Returns Integer*
+  * string.guid() *Generates Unique ID string*
 
 <h3>In the future?</h3>
 
@@ -47,4 +65,4 @@ Hopefully, in the future I will extend this to work with other objects and not j
 
 Copyright Nijikokun 2011 <nijikokun@gmail.com>
 
-GNUv3 Affero License <http://www.gnu.org/licenses/agpl-3.0.html>
+AOL License <http://aol.nexua.org>
--- estro.js
+++ estro.js
@@ -4,27 +4,200 @@
  *
  * @a: Nijikokun <nijikokun@gmail.com>
  * @c: 2011 (c) Nijikokun
- * @l: GNUv3 Affero License <http://www.gnu.org/licenses/agpl-3.0.html>
+ * @l: AOL License <http://aol.nexua.org>
  */
-(function(){
-    var methods = {
-        isEmpty: function(){ var o=this,u=undefined; return (!o||o===''||o===u||o===null||o.length < 1||o==='undefined'||(typeof o)===u); },
-        compareTo: function(s) { var o=this,x=o.length,z=s.length,n=(x<z?x:z); for(var i=0;i<n;i++) { var a=o.charCodeAt(i), b=s.charCodeAt(i); if(a!=b) return (a-b); } return (x-z); },
-        startsWith: function (str) { return !this.indexOf(str); },
-        endsWith: function(str) { return this.indexOf(str, this.length - str.length)!==-1; },
-        contains: function(n) {  return this.indexOf(n)!=-1; },
-        equals: function(str) { return (this == str); },
-        equalsIgnoreCase: function(str) { var o=this; return (o==str) ? true : ((!str.isEmpty()) && (str.length==o.length) && o.toLowerCase() == str.toLowerCase()); },
-        trim: function() { return this.replace(/^\s\s*/, '').replace(/\s\s*$/, ''); },
-        repeat: function(n){ return new Array(n?n+1:2).join(this); },
-        reverse: function () { return this.split('').reverse().join(''); },
-        capitalize: function() { var w=this.split(' '); for(var i=0;i<w.length;i++) { w[i]=w[i].charAt(0).toUpperCase()+w[i].substring(1).toLowerCase(); } return w.join(" "); },
-        camelize: function() { return this.replace(/\-(\w)/g, function(a, b) { return b.toUpperCase(); }); },
-        bind: function (d) { var m,o=this; while (m=/%\{\s*([^\}\s]+)\s*\}/.exec(o)) { o=o.replace(m[0], d[m[1]] || '??'); } return o; },
-        extract: function(rgx, n) { n=(n === undefined)?0:n; if (!rgx.global) { return this.match(rgx)[n] || ''; } var m,e=[]; while((m=rgx.exec(this))) { e[e.length] = m[n] || ''; } return e; }
+(function () {
+    var Estro = {
+        estro: function() {
+            return "Version 2.1"
+        },
+        
+        isEmpty: function () {
+            return (!this || 0 === this.length);
+        },
+        
+        isBlank: function() {
+            return (!this || /^\s*$/.test(this));
+        },
+        
+        compareTo: function (s) {
+            var o = this,
+                x = o.length,
+                z = s.length,
+                n = (x < z ? x : z);
+                
+            for (var i = 0; i < n; i++) {
+                var a = o.charCodeAt(i), b = s.charCodeAt(i);
+                
+                if (a != b) 
+                    return (a - b);
+            }
+            
+            return (x - z);
+        },
+        
+        startsWith: function (str) {
+            return !this.indexOf(str);
+        },
+        
+        endsWith: function (str) {
+            return this.indexOf(str, this.length - str.length) !== -1;
+        },
+        
+        contains: function (n) {
+            return this.indexOf(n) != -1;
+        },
+        
+        equals: function (str) {
+            return (this == str);
+        },
+        
+        equalsIgnoreCase: function (str) {
+            var o = this;
+            return (o == str) ? true : ((!str.isEmpty()) && (str.length == o.length) && o.toLowerCase() == str.toLowerCase());
+        },
+        
+        trim: function () {
+            return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        },
+        
+        trimLeft: function () {
+            return this.trimLeft ? this.trimLeft() : this.replace(/^\s+/, '');
+        },
+
+        trimRight: function () {
+            return this.trimRight ? this.trimRight() : this.replace(/\s+$/, '');
+        },
+        
+        trimNonAlpha: function () {
+            return this.replace(/[^A-Za-z ]+/g, '');
+        },
+        
+        trimNonAlphaNumeric: function () {
+            return this.replace(/[^A-Za-z0-9 ]+/g, '');
+        },
+        
+        trimNonNumeric: function () {
+            return this.replace(/[^0-9-.]/g, '');
+        },
+        
+        trimNumeric: function () {
+            return this.replace(/[0-9]/g, '');
+        },
+        
+        repeat: function (n) {
+            return new Array(n ? n + 1 : 2).join(this);
+        },
+        
+        reverse: function () {
+            return this.split('').reverse().join('');
+        },
+
+        insert: function (s, i) {
+            return this.slice(0, i) + s + this.slice(i);
+        },
+        
+        remove: function (s, e) {
+            return this.slice(0, s) + this.slice(e);
+        },
+        
+        chop: function (a) {
+            return this.slice(0, a ? a<0 ? a : a*-1 : -1);
+        },
+        
+        capitalize: function () {
+            return this.charAt(0).toUpperCase() + this.slice(1);
+        },
+        
+        uncapitalize: function () {
+            return this.charAt(0).toLowerCase() + this.slice(1);
+        },
+        
+        capitalizeWords: function () {
+            return this.replace(/\w+/g, function (w) {
+                return w.capitalize();
+            });
+        },
+
+        uncapitalizeWords: function () {
+            return this.replace(/\w+/g, function (w) {
+                return w.uncapitalize();
+            });
+        },
+
+        isUpperCaseAt: function (i) {
+            return this.charAt(i).toUpperCase() === this.charAt(i);
+        },
+
+        isLowerCaseAt: function (i) {
+            return this.charAt(i).toLowerCase() === this.charAt(i);
+        },
+        
+        swapCase: function () {
+            return this.replace(/([a-z]+)|([A-Z]+)/g, function (m, l, u) {
+                return l ? m.toUpperCase() : m.toLowerCase();
+            });
+        },
+        
+        camilize: function() {
+            return input.replace(/\W+(.)/g, function (m, l) {
+                return l.toUpperCase();
+            });
+        },
+        
+        dasherize: function () {
+            return this.replace(/\W+/g, '-').replace(/([a-z\d])([A-Z])/g, '$1-$2').toLowerCase();
+        },
+        
+        underscore: function () {
+            return this.replace(/\W+/g, '_').replace(/([a-z\d])([A-Z])/g, '$1_$2').toLowerCase();
+        },
+        
+        bind: function (d) {
+            var m, o = this;
+            while (m = /%\{\s*([^\}\s]+)\s*\}/.exec(o)) {
+                o = o.replace(m[0], d[m[1]] || '??');
+            }
+            return o;
+        },
+        
+        extract: function (rgx, n) {
+            n = (n === undefined) ? 0 : n;
+            if (!rgx.global) {
+                return this.match(rgx)[n] || '';
+            }
+            var m, e = [];
+            while ((m = rgx.exec(this))) {
+                e[e.length] = m[n] || '';
+            }
+            return e;
+        },
+        
+        guid: function (l) {
+            var b = [], c = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', cl = c.length;
+            
+            for (var i = 0; i < (l || 32); i++)
+                b[i] = c.charAt(Math.floor(Math.random() * cl));
+                
+            return b.join('');
+        },
+        
+        toInt: function () {
+            for(var b=[],a=0;a<this.length;a++)
+                b[a] = this.charCodeAt(a);
+                
+            return b
+        },
+        
+        toHash: function() {
+            var h = 0, l = this.length();
+            for (var i = 0; i < l; i++)
+                h = 31 * h + this.charAt(i);
+            return h; 
+        }
     };
     
-    for (var m in methods)
-        if(!String.prototype[m])
-            String.prototype[m] = methods[m];
+    for (var e in Estro)
+        if (!String.prototype[e]) 
+            String.prototype[e] = methods[e];
 })();
